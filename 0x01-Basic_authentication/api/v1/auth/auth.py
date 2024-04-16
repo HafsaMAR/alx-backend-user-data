7a81ae8dc544010985c88b82c7fdda03cb1aba5a
HafsaMAR <marouche.hafsa@gmail.com>
2024-04-16 22:12:58 +0100

    request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 '''
 
 
+import re
 from typing import List, TypeVar
 from flask import request
 
@@ -19,16 +20,32 @@ class Auth:
         """
         if path is None or not excluded_paths:
             return True
-        for excluded_path in excluded_paths:
-            if path.startswith(excluded_path):
-                return False
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
         """
         Get the authorization header from the flask request.
+        Returns None if request is None or
+        if the header key Authorization is not present.
+        Otherwise, returns the value of the Authorization header.
         """
-        return None
+        if request is None:
+            return None
+        auth_header = request.headers.get('Authorization')
+        if not auth_header:
+            return None
+        return auth_header
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
