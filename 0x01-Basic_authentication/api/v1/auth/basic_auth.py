9fb393023c3c6c5f8df24829cd1759c816f7219d
HafsaMAR <marouche.hafsa@gmail.com>
2024-04-18 18:30:02 +0100

    return user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -44,3 +44,19 @@ class BasicAuth(Auth):
         except binascii.Error:
             # decoding actually failed
             return None
+
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        ''' Return the credentials of the user
+        '''
+        if decoded_base64_authorization_header is None or not isinstance(
+                decoded_base64_authorization_header, str):
+            return None, None
+        
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        username, password = decoded_base64_authorization_header.split(':', 1)
+        return username.strip(), password.strip()
+        
+        
+        
