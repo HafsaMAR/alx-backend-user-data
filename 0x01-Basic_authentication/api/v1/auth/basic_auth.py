39aace167bc1f681999d8f622db0e9bfe3676b9d
HafsaMAR <marouche.hafsa@gmail.com>
2024-04-18 18:10:34 +0100

    decode the base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -21,7 +21,26 @@ class BasicAuth(Auth):
         """
         if type(authorization_header) == str:
             pattern = r'Basic (?P<token>.+)'
-            field_match = re.fullmatch(pattern, authorization_header.strip())
-            if field_match is not None:
-                return field_match.group('token')
+            match = re.fullmatch(pattern, authorization_header.strip())
+            if match is not None:
+                return match.group('token')
         return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        ''' decode the base64 string
+        '''
+        if base64_authorization_header is None or type(
+                base64_authorization_header) != str:
+            return None
+
+        try:
+            # decode the base64-encoded to bytes
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            # decode the bytes to a UTF-8 string
+            decoded_string = decoded_bytes.decode('utf_8')
+            return decoded_string
+        except binascii.Error:
+            # decoding actually failed
+            return None
