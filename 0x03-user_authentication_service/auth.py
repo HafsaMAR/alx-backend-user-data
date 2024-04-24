7c4ede6446804d350d244c018c3c7de1bec6712d
HafsaMAR <marouche.hafsa@gmail.com>
2024-04-24 20:50:52 +0100

    password hashing method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+'''authentication module and hashing
+'''
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
