457a847dffb7b88d9e8c3244ced968df30ea8052
HafsaMAR <marouche.hafsa@gmail.com>
2024-04-24 21:20:02 +0100

    pycodestyle check

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -7,10 +7,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def welcome():
     '''welcome method'''
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
