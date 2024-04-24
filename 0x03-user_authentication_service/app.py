b30b5193f19a5915efb2e1b5b71c2365fdee2984
HafsaMAR <marouche.hafsa@gmail.com>
2024-04-24 21:17:28 +0100

    start the flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+'''flask app module'''
+
+
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/', method='GET', strict_slashes=False)
+def welcome():
+    '''welcome method'''
+    return jsonify({"message": "Bienvenue"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
