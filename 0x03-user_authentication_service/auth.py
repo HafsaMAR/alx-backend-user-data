cffc83f68698df8537f0139c2dcd930b15237f48
HafsaMAR <marouche.hafsa@gmail.com>
2024-04-24 22:06:20 +0100

    authentication login _ Auth method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -36,6 +36,7 @@ class Auth:
     def valid_login(self, email: str, password: str):
         '''check credentials
         '''
+        user = None
         try:
             user = self._db.find_user_by(email=email)
             hashed_password = user.hashed_password
